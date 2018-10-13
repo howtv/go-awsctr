4ce84424f0574d3182795cf76cc91ddd1c2c1202
komapotter <36rafts@gmail.com>
2018-10-13 11:03:27 +0900

    [fix] nil pointer dereference

--- cloudfront.go
+++ cloudfront.go
@@ -40,29 +40,29 @@ type cloudFrontImpl struct {
 // aws cloudfront create-invalidation --distribution-id S11A16G5KZMEQD --paths /index.html /error.html
 func (c *cloudFrontImpl) Invalidate(iv InvalidateInfo) error {
 	path := aws.String(iv.Path)
-	var paths *cloudfront.Paths
-	paths = paths.SetQuantity(1)
-	paths = paths.SetItems([]*string{path})
+	var paths cloudfront.Paths
+	paths = *paths.SetQuantity(1)
+	paths = *paths.SetItems([]*string{path})
 	if err := paths.Validate(); err != nil {
 		return err
 	}
 
 	unixTime := time.Now().Unix()
-	var batch *cloudfront.InvalidationBatch
-	batch = batch.SetCallerReference(fmt.Sprint(unixTime))
-	batch = batch.SetPaths(paths)
+	var batch cloudfront.InvalidationBatch
+	batch = *batch.SetCallerReference(fmt.Sprint(unixTime))
+	batch = *batch.SetPaths(&paths)
 	if err := batch.Validate(); err != nil {
 		return err
 	}
 
-	var input *cloudfront.CreateInvalidationInput
-	input = input.SetDistributionId(iv.DistID)
-	input = input.SetInvalidationBatch(batch)
+	var input cloudfront.CreateInvalidationInput
+	input = *input.SetDistributionId(iv.DistID)
+	input = *input.SetInvalidationBatch(&batch)
 	if err := input.Validate(); err != nil {
 		return err
 	}
 
-	if _, err := c.client.CreateInvalidation(input); err != nil {
+	if _, err := c.client.CreateInvalidation(&input); err != nil {
 		return err
 	}
 
